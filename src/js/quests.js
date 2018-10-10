a574c84a42a9dcb2426a981e3b5240542ecd9dc1
U-SHADOWMOON\Jacob <jacobjdowning@gmail.com>
2018-10-10 16:47:01 -0400

    updated quests

--- src/js/quests.js
+++ src/js/quests.js
@@ -1,45 +1,21 @@
 export default
-[	
+[		
 	{
-		"itemID": "158203",
-		"itemName": "Incendiary Ammunition",
-		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_eng_incendiaryammunition.jpg",
-		"count": "2"
-	},
+		"itemID": "152495",
+		"itemName": "Coastal Mana Potion",
+		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_alchemy_80_potion01blue.jpg",
+		"count": "20"
+	},	
 	{
-		"itemID": "152812",
-		"itemName": "Monel-Hardened Hoofplates",
-		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_blacksmith_leystonehoofplates.jpg",
-		"count": "2"
-	},
-	{
-		"itemID": "152509",
-		"itemName": "Siren's Pollen",
-		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_misc_herb_pollen.jpg",
-		"count": "60"
-	},
-	{
-		"itemID": "153715",
-		"itemName": "Straddling Viridium",
-		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_jewelcrafting_80_cutgem01_green.jpg",
+		"itemID": "153710",
+		"itemName": "Deadly Solstone",
+		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_jewelcrafting_80_cutgem01_orange.jpg",
 		"count": "15"
 	},
 	{
-		"itemID": "154707",
-		"itemName": "Battle Flag: Spirit of Freedom",
-		"icon": "https://wow.zamimg.com/images/wow/icons/large/spell_tailor_ccreduce01.jpg",
-		"count": "1"
-	},	
-	{
-		"itemID": "153438",
-		"itemName": "Enchant Ring - Seal of Critical Strike",
-		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_misc_enchantedscroll.jpg",
-		"count": "3"
-	},
-	{
-		"itemID": "152541",
-		"itemName": "Coarse Leather",
-		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_skinning_80_coarseleather.jpg",
+		"itemID": "152512",
+		"itemName": "Monelite Ore",
+		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_ore_monalite.jpg",
 		"count": "60"
 	},
 	{
@@ -47,11 +23,35 @@ export default
 		"itemName": "War-Scroll of Intellect",
 		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_inscription_80_warscroll_intellect.jpg",
 		"count": "3"
+	},	
+	{
+		"itemID": "154707",
+		"itemName": "Battle Flag: Spirit of Freedom",
+		"icon": "https://wow.zamimg.com/images/wow/icons/large/spell_tailor_ccreduce01.jpg",
+		"count": "1"
 	},
 	{
-		"itemID": "152547",
-		"itemName": "Great Sea Catfish",
-		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_fishing_greatseacatfish.jpg",
+		"itemID": "153490",
+		"itemName": "F.R.I.E.D.",
+		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_eng_bombfire.jpg",
+		"count": "6"
+	},
+	{
+		"itemID": "154889",
+		"itemName": "Grilled Catfish",
+		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_cooking_80_grilledsnapper.jpg",
 		"count": "60"
+	},
+	{
+		"itemID": "154167",
+		"itemName": "Drums of the Maelstrom",
+		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_leatherworking_drumsofthemaestrom.jpg",
+		"count": "2"
+	},	
+	{
+		"itemID": "159789",
+		"itemName": "Enchant Weapon - Stalwart Navigation",
+		"icon": "https://wow.zamimg.com/images/wow/icons/large/inv_misc_enchantedscroll.jpg",
+		"count": "3"
 	}
 ]
